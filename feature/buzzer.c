fd99cc5acba34656284c0a2506afd9f241602317
Leo-Luz-Code <leonardoluz768@gmail.com>
2025-01-19 17:11:07 -0300

    feat: add buzzer implementation

--- feature/buzzer.c
+++ feature/buzzer.c
@@ -1,16 +1,31 @@
 #include "../include/buzzer.h"
+#include "pico/stdlib.h"
 
-// altere conforme for necessario
+#define BUZZER_PIN 21
+
+// inicializa o buzzer
 void buzzer_init(void)
 {
+    gpio_init(BUZZER_PIN);
+    gpio_set_dir(BUZZER_PIN, GPIO_OUT);
+    gpio_put(BUZZER_PIN, 0);
 }
 
-// Liga o buzzer
+// liga o buzzer
 void buzzer_on(void)
 {
+    gpio_put(BUZZER_PIN, 1);
 }
 
 // Desliga o buzzer
 void buzzer_off(void)
 {
+    gpio_put(BUZZER_PIN, 0);
+}
+
+void buzzer_beep(void)
+{
+    buzzer_on();
+    sleep_ms(500);
+    buzzer_off();
 }
