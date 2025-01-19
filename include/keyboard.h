15e37d44c24a2b7d7f293b580068e36c358e1075
Leo-Luz-Code <leonardoluz768@gmail.com>
2025-01-19 16:50:27 -0300

    feat: add led implementation
    also include pico libs in keyboard.h

--- include/keyboard.h
+++ include/keyboard.h
@@ -1,7 +1,11 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
-// altere conforme necessario
+#include "pico/stdlib.h"
+
+#define ROWS 4
+#define COLS 4
+
 void keyboard_init();
 char keyboard_get_key();
 
--- include/led.h
+++ include/led.h
@@ -1,9 +1,26 @@
 #ifndef LED_H
 #define LED_H
 
-// altere conforme necessario
-void led_init(void);
-void led_on(int led_id);
-void led_off(int led_id);
+#include "pico/stdlib.h"
+
+#define RED 12
+#define GREEN 13
+#define BLUE 11
+
+#define LED_ON 1
+#define LED_OFF 0
+
+void init_all_leds(void);
+
+void turn_on_red(void);
+void turn_on_green(void);
+void turn_on_blue(void);
+
+void turn_off_red(void);
+void turn_off_green(void);
+void turn_off_blue(void);
+
+void turn_on_all_leds(void);
+void turn_off_all_leds(void);
 
 #endif
